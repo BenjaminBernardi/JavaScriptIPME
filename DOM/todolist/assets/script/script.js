2a49fbb14ea62b26a06cd1b5d2423b63e4fcc31b
BenjaminBernardi <benjamin.bernardi@outlook.com>
2025-05-28 12:02:57 +0200

    save progress

--- DOM/todolist/assets/script/script.js
+++ DOM/todolist/assets/script/script.js
@@ -1,4 +1,26 @@
-let textToDO = document.querySelector(".texte-do");
-let addToDO = document.querySelector(".ajouter-todo");
+let inputTextToDO = document.querySelector(".texte-todo");
+let buttonAddToDO = document.querySelector(".ajouter-todo");
 let deleteAll = document.querySelector(".supprimer-tout");
-let myToDo = document.querySelectorAll(".mes-todo");
+let myToDo = document.querySelector(".mes-todo");
+
+buttonAddToDO.addEventListener("click", () => {
+    // Créer un élément li avec createElement
+    let newLi = document.createElement("li");
+    // Insérer ma valeur dedans
+    let newToDo = document.createTextNode(inputTextToDO.value);
+    
+
+
+
+    // Ajouter un evenement click sur le li
+        // regarder si la class existe sur mon element
+            // si oui -> je la supprime
+            // si non -> li classList add 
+
+
+
+
+    // Inserer l'élément dans myToDo avec appendChild
+    newLi.appendChild(newToDo);
+    document.body.insertBefore(newLi, myToDo);
+});
