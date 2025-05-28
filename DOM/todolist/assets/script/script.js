241203614dc1358f4a71a0d5cd1560daedfd499a
BenjaminBernardi <benjamin.bernardi@outlook.com>
2025-05-28 10:01:22 +0200

    save progress

---
+++ DOM/todolist/assets/script/script.js
@@ -0,0 +1,4 @@
+let textToDO = document.querySelector(".texte-do");
+let addToDO = document.querySelector(".ajouter-todo");
+let deleteAll = document.querySelector(".supprimer-tout");
+let myToDo = document.querySelectorAll(".mes-todo");
