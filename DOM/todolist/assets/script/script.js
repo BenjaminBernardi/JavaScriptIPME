ba61316c36075ea6aa12ce01fafe4692ed830ff0
BenjaminBernardi <benjamin.bernardi@outlook.com>
2025-05-28 14:12:57 +0200

    save progress

--- DOM/todolist/assets/script/script.js
+++ DOM/todolist/assets/script/script.js
@@ -4,23 +4,26 @@ let deleteAll = document.querySelector(".supprimer-tout");
 let myToDo = document.querySelector(".mes-todo");
 
 buttonAddToDO.addEventListener("click", () => {
-    // Créer un élément li avec createElement
-    let newLi = document.createElement("li");
-    // Insérer ma valeur dedans
-    let newToDo = document.createTextNode(inputTextToDO.value);
-    
+  // Créer un élément li avec createElement
+  let newLi = document.createElement("li");
+  // Insérer ma valeur dedans
+  let newToDo = document.createTextNode(inputTextToDO.value);
+  //ajouter une classe de base
+  newLi.classList.add("styleToDo");
 
+  // Ajouter un evenement click sur le li
+  newLi.addEventListener("click", () => {
+    // regarder si la class existe sur mon element
+    if (newLi.classList.contains("lineThrough")) {
+      // si oui -> je la supprime
+      newLi.classList.remove("lineThrough");
+    } else {
+      // si non -> li classList add
+      newLi.classList.add("lineThrough");
+    }
+  });
 
-
-    // Ajouter un evenement click sur le li
-        // regarder si la class existe sur mon element
-            // si oui -> je la supprime
-            // si non -> li classList add 
-
-
-
-
-    // Inserer l'élément dans myToDo avec appendChild
-    newLi.appendChild(newToDo);
-    document.body.insertBefore(newLi, myToDo);
+  // Inserer l'élément dans myToDo avec appendChild
+  newLi.appendChild(newToDo);
+  myToDo.append(newLi);
 });
