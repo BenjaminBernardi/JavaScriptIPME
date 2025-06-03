1666e03f7e38a10511ccd88e72a4e512a8bacdae
BenjaminBernardi <benjamin.bernardi@outlook.com>
2025-06-03 12:02:30 +0200

    save progress

--- DOM/duel de sorciers/assets/script/script.js
+++ DOM/duel de sorciers/assets/script/script.js
@@ -4,6 +4,7 @@ let inputMaison1 = document.querySelector(".maison-sorcier-1");
 let inputSorcier2 = document.querySelector(".nom-sorcier-2");
 let inputMaison2 = document.querySelector(".maison-sorcier-2");
 let displayPlayersStatus = document.querySelector(".players-status");
+let displayBattleStatus = document.querySelector(".battle-status");
 
 const playerOne = {
   maxHP: 200,
@@ -28,16 +29,44 @@ form.addEventListener("submit", (e) => {
 
 function displayBattleInfo() {
   playersStats();
+  battleStatus();
 }
 
 function playersStats() {
   let newP1 = document.createElement("p");
-  newP1.innerHTML = playerOne.name;
+  newP1.innerHTML = playerOne.name + ": " + playerOne.house;
   let newP2 = document.createElement("p");
-  newP2.innerHTML = playerOne.house;
-  let newP3 = document.createElement("p");
-  newP3.innerHTML = playerTwo.name;
-  let newP4 = document.createElement("p");
-  newP4.innerHTML = playerTwo.house;
-  displayPlayersStatus.append(newP1, newP2, newP3, newP4);
+  newP2.innerHTML = playerTwo.name + ": " + playerTwo.house;
+  displayPlayersStatus.append(newP1, newP2);
+}
+
+function battleStatus() {
+  let newH2 = document.createElement("h2");
+  newH2.innerHTML = "Historique du duel :";
+  displayBattleStatus.append(newH2);
+
+  while(playerOne.actualHP > 0 && playerTwo.actualHP > 0) {
+    attack();
+  }
+}
+
+function attack() {
+    let damage1 = Math.floor(Math.random() * 10) + 5;
+    playerTwo.actualHP -= damage1;
+    if (playerTwo.actualHP > 0 && playerOne.actualHP > 0) {
+        console.log("Duel : " + playerOne.name + " attaque " + playerTwo.name + " pour " + damage1 + " dégâts. Il reste " + playerTwo.actualHP + " PV à " + playerTwo.name + ".");
+    } else if (playerTwo.actualHP <= 0 && playerOne.actualHP > 0) {
+        playerTwo.actualHP = 0;
+        console.log("Duel : " + playerOne.name + " attaque " + playerTwo.name + " pour " + damage1 + " dégâts. Il reste " + playerTwo.actualHP + " PV à " + playerTwo.name + ".");
+        console.log(playerOne.name + " (" + playerOne.house + ") a vaincu " + playerTwo.name + " (" + playerTwo.house + ") !");
+    }
+    let damage2 = Math.floor(Math.random() * 10) + 5;
+    playerOne.actualHP -= damage2;
+    if (playerOne.actualHP > 0) {
+        console.log("Duel : " + playerTwo.name + " attaque " + playerOne.name + " pour " + damage2 + " dégâts. Il reste " + playerOne.actualHP + " PV à " + playerOne.name + ".");
+    } else {
+        playerOne.actualHP = 0;
+        console.log("Duel : " + playerTwo.name + " attaque " + playerOne.name + " pour " + damage2 + " dégâts. Il reste " + playerOne.actualHP + " PV à " + playerOne.name + ".");
+        console.log(playerTwo.name + " (" + playerTwo.house + ") a vaincu " + playerOne.name + " (" + playerOne.house + ") !");
+    }
 }
